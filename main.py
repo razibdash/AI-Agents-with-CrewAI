7a21e28f6888910993bbb8398710d2e84cdfeed6
razibdash <avrorazib@gmail.com>
2025-07-19 01:00:05 +0600

    define the writer task

--- main.py
+++ main.py
@@ -61,3 +61,18 @@ research_task = Task(
     agent=researcher,
 )
 
+#define the writer task
+writter_task=Task(
+    description=(
+        "Compose an insightful article on {topic}. "
+        "Focus on the latest trends and how it's impacting the industry. "
+        "This article should be easy to understand, engaging, and positive."
+    ),
+    expected_output="A 4 paragraph article on {topic} advancements formatted as markdown.",
+    tools=[search_tool],
+    agent=writer,
+    async_execution=False,
+    output_file="new-blog-post.md",
+)
+
+
